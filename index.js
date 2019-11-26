9563c5559ced219b13822ff200be2f731eabb60f
MFeiz <30654631+mohammadFeiz@users.noreply.github.com>
2019-11-26 12:27:48 -0800

    .

--- index.js
+++ index.js
@@ -13,135 +13,252 @@ function _instanceof(left, right) { if (right != null && typeof Symbol !== "unde
 
 function _classCallCheck(instance, Constructor) { if (!_instanceof(instance, Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+var RActions = function RActions() {
+  var _this = this;
 
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+  _classCallCheck(this, RActions);
 
-var RActions =
-/*#__PURE__*/
-function () {
-  function RActions() {
-    _classCallCheck(this, RActions);
-  }
+  this.getValueByField = function (obj, field) {
+    if (!field) {
+      return undefined;
+    }
 
-  _createClass(RActions, [{
-    key: "getValueByField",
-    value: function getValueByField(obj, field) {
-      if (!field) {
-        return undefined;
-      }
+    var fields = (typeof field === 'function' ? field(obj) : field).split('.');
+    var value = obj[fields[0]];
 
-      var fields = (typeof field === 'function' ? field(obj) : field).split('.');
-      var value = obj[fields[0]];
+    if (value === undefined) {
+      return;
+    }
+
+    for (var i = 1; i < fields.length; i++) {
+      value = value[fields[i]];
 
       if (value === undefined) {
         return;
       }
+    }
 
-      for (var i = 1; i < fields.length; i++) {
-        value = value[fields[i]];
+    return value;
+  };
 
-        if (value === undefined) {
-          return;
-        }
+  this.getCopy = function (obj) {
+    return JSON.parse(JSON.stringify(obj));
+  };
+
+  this.setValueByField = function (obj, field, value) {
+    var fields = field.split('.');
+    var node = obj;
+
+    for (var i = 0; i < fields.length - 1; i++) {
+      if (node[fields[i]] === undefined) {
+        return;
       }
 
-      return value;
+      node = node[fields[i]];
     }
-  }, {
-    key: "setValueByField",
-    value: function setValueByField(obj, field, value) {
-      var fields = field.split('.');
-      var node = obj;
 
-      for (var i = 0; i < fields.length - 1; i++) {
-        if (node[fields[i]] === undefined) {
-          return;
-        }
+    node[fields[fields.length - 1]] = value;
+    return obj;
+  };
 
-        node = node[fields[i]];
+  this.eventHandler = function (selector, event, action) {
+    var type = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : 'bind';
+    var me = {
+      mousedown: "touchstart",
+      mousemove: "touchmove",
+      mouseup: "touchend"
+    };
+    event = 'ontouchstart' in document.documentElement ? me[event] : event;
+    var element = typeof selector === "string" ? selector === "window" ? (0, _jquery.default)(window) : (0, _jquery.default)(selector) : selector;
+    element.unbind(event, action);
+
+    if (type === 'bind') {
+      element.bind(event, action);
+    }
+  };
+
+  this.getClient = function (e) {
+    return {
+      x: e.clientX === undefined ? e.changedTouches[0].clientX : e.clientX,
+      y: e.clientY === undefined ? e.changedTouches[0].clientY : e.clientY
+    };
+  };
+
+  this.getLineBySMA = function (_ref) {
+    var p1 = _ref.p1,
+        measure = _ref.measure,
+        angle = _ref.angle;
+    return {
+      p1: p1,
+      p2: {
+        x: p1.x + Math.cos(angle * Math.PI / 180) * measure,
+        y: p1.y + Math.sin(angle * -1 * Math.PI / 180) * measure
       }
+    };
+  };
 
-      node[fields[fields.length - 1]] = value;
-      return obj;
-    }
-  }, {
-    key: "eventHandler",
-    value: function eventHandler(selector, event, action) {
-      var type = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : 'bind';
-      var me = {
-        mousedown: "touchstart",
-        mousemove: "touchmove",
-        mouseup: "touchend"
-      };
-      event = 'ontouchstart' in document.documentElement ? me[event] : event;
-      var element = typeof selector === "string" ? selector === "window" ? (0, _jquery.default)(window) : (0, _jquery.default)(selector) : selector;
-      element.unbind(event, action);
+  this.getValueByRange = function (value, start, end) {
+    var val;
 
-      if (type === 'bind') {
-        element.bind(event, action);
-      }
+    if (value === undefined) {
+      return start;
     }
-  }, {
-    key: "getClient",
-    value: function getClient(e) {
-      return {
-        x: e.clientX === undefined ? e.changedTouches[0].clientX : e.clientX,
-        y: e.clientY === undefined ? e.changedTouches[0].clientY : e.clientY
-      };
-    }
-  }, {
-    key: "getLineBySMA",
-    value: function getLineBySMA(_ref) {
-      var p1 = _ref.p1,
-          measure = _ref.measure,
-          angle = _ref.angle;
-      return {
-        p1: p1,
-        p2: {
-          x: p1.x + Math.cos(angle * Math.PI / 180) * measure,
-          y: p1.y + Math.sin(angle * -1 * Math.PI / 180) * measure
-        }
-      };
-    }
-  }, {
-    key: "getValueByRange",
-    value: function getValueByRange(value, start, end) {
-      var val;
 
-      if (value === undefined) {
-        return start;
-      }
-
-      if (typeof value === 'number') {
-        val = value;
+    if (typeof value === 'number') {
+      val = value;
+    } else {
+      if (value.indexOf('%') !== -1) {
+        var range = end - start;
+        val = range * parseFloat(value) / 100 + start;
       } else {
-        if (value.indexOf('%') !== -1) {
-          var range = end - start;
-          val = range * parseFloat(value) / 100 + start;
-        } else {
-          val = parseFloat(value);
+        val = parseFloat(value);
+      }
+    }
+
+    val = val > end ? end : val;
+    val = val < start ? start : val;
+    return val;
+  };
+
+  this.getPercentByValue = function (value, start, end) {
+    return 100 * (value - start) / (end - start);
+  };
+
+  this.getValueByPercent = function (percent, start, end) {
+    return start + percent * (end - start) / 100;
+  };
+
+  this.getStartByStep = function (start, step) {
+    var a = Math.round((start - step) / step) * step;
+
+    while (a < start) {
+      a += step;
+    }
+
+    return a;
+  };
+
+  this.fix = function (number) {
+    var a = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 6;
+    return parseFloat(number.toFixed(a));
+  };
+
+  this.searchComposite = function (model, query) {
+    var childsProp = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : 'childs';
+
+    var searchRowRecursive = function searchRowRecursive(data, query) {
+      if (_this.searchResult !== undefined) {
+        return;
+      }
+
+      for (var i = 0; i < data.length; i++) {
+        if (_this.searchResult !== undefined) {
+          break;
+        }
+
+        var row = data[i];
+
+        for (var prop in query) {
+          var value = _this.getValueByField(row, prop);
+
+          if (value !== query[prop]) {
+            continue;
+          }
+
+          _this.searchResult = row;
+          break;
+        }
+
+        if (row[childsProp] && row[childsProp].length) {
+          searchRowRecursive(row[childsProp], query);
         }
       }
+    };
 
-      val = val > end ? end : val;
-      val = val < start ? start : val;
-      return val;
+    _this.searchResult = undefined;
+    searchRowRecursive(model, query);
+    return _this.searchResult;
+  };
+
+  this.convertFlatToComposite = function (model) {
+    var idProp = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 'id';
+    var parentIdProp = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : 'parentId';
+
+    var convertModelRecursive = function convertModelRecursive(model, parentId, parentObject) {
+      for (var i = 0; i < model.length; i++) {
+        var row = model[i];
+        row._parent = _this.getValueByField(row, parentIdProp);
+
+        if (row._parent !== parentId) {
+          continue;
+        }
+
+        row._id = _this.getValueByField(row, idProp);
+        row._childs = [];
+        parentObject.push(row);
+        convertModelRecursive(model, row._id, row._childs);
+      }
+    };
+
+    var result = [];
+    convertModelRecursive(model, undefined, result);
+    return result;
+  };
+
+  this.compaire = function (a, b) {
+    return JSON.stringify(a) === JSON.stringify(b);
+  };
+
+  this.binarySearch = function (arr, value, field) {
+    var limit = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : 0;
+    var start = 0,
+        end = arr.length - 1;
+    var startValue = field(arr[start]);
+    var endValue = field(arr[end]);
+
+    if (value < startValue) {
+      return Math.abs(value - startValue) <= limit ? start : -1;
     }
-  }, {
-    key: "getStartByStep",
-    value: function getStartByStep(start, step) {
-      var a = Math.round((start - step) / step) * step;
 
-      while (a < start) {
-        a += step;
+    if (value > endValue) {
+      return Math.abs(value - endValue) <= limit ? end : -1;
+    }
+
+    if (value === startValue) {
+      return start;
+    }
+
+    if (value === endValue) {
+      return end;
+    }
+
+    while (end - start > 1) {
+      var mid = Math.floor((end + start) / 2);
+      var mp = field(arr[mid]);
+      var dif = value - mp;
+
+      if (dif === 0) {
+        return mid;
       }
 
-      return a;
-    }
-  }]);
+      if (dif < 0) {
+        end = mid;
+      } //اگر مقدار در سمت چپ است
+      else {
+          start = mid;
+        } //اگر مقدار در سمت راست است
 
-  return RActions;
-}();
+    }
+
+    var startDif = Math.abs(field(arr[start]) - value);
+    var endDif = Math.abs(field(arr[end]) - value);
+
+    if (startDif <= endDif) {
+      return startDif <= limit ? start : -1;
+    } else {
+      return endDif <= limit ? end : -1;
+    }
+  };
+};
 
 exports.default = RActions;
